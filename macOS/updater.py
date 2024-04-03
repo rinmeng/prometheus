513cba31f3745a7daefe17f1e6d6360275933b45
rin! <phurinmeng@gmail.com>
2024-04-02 18:57:51 -0700

    Update macOS updater.py script

--- macOS/updater.py
+++ macOS/updater.py
@@ -9,23 +9,23 @@ import os
 def update_prometheus():
     url = "https://raw.githubusercontent.com/rin-williams/prometheus/main/macOS/prometheus.py"
     response = requests.get(url)
-    with open("macOS/prometheus.py", "w") as file:
+    with open("prometheus.py", "w") as file:
         file.write(response.text)
     print("Prometheus has been updated.")
 
 
 def update_chronos():
-    url = "https://raw.githubusercontent.com/rin-williams/chronos/main/macOS/dependencies/chronos.py"
+    url = "https://raw.githubusercontent.com/rin-williams/chronos/main/macOS/chronos.py"
     response = requests.get(url)
-    with open("macOS/dependencies/chronos.py", "w") as file:
+    with open("chronos.py", "w") as file:
         file.write(response.text)
     print("Chronos has been updated.")
 
 
 def update_dependencies():
-    url = "https://raw.githubusercontent.com/rin-williams/chronos/main/macOS/dependencies/dependencies.py"
+    url = "https://raw.githubusercontent.com/rin-williams/chronos/main/macOS/dependencies.py"
     response = requests.get(url)
-    with open("macOS/dependencies/dependencies.py", "w") as file:
+    with open("dependencies.py", "w") as file:
         file.write(response.text)
     print("Dependencies has been updated.")
 
